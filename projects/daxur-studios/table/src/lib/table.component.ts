a7744029686ac963559215a18c0c529ab18392e6
Bruno Kertesz <daxur.studios@gmail.com>
2024-01-20 17:52:38 +0000

    - initial commit, demo app and library

---
+++ projects/daxur-studios/table/src/lib/table.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-table',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      table works!
+    </p>
+  `,
+  styles: ``
+})
+export class TableComponent {
+
+}
